2df349d06ef50325cf03976d56ff448f8dff9ac9
Justin Kwok <jkwok0714@gmail.com>
2017-12-14 15:37:32 -0800

    Additional navigation

--- client/src/components/BeerView.jsx
+++ client/src/components/BeerView.jsx
@@ -6,8 +6,16 @@ const BeerView = (props) => {
   };
 
   return (
-    <div onClick={handleClick}>
+    <div>
       <h2>{props.beer.name}</h2>
+      <div>{props.beer.date}</div>
+      <div>{props.beer.brewery}</div>
+      <div>{props.beer.style}</div>
+      <div>{props.beer.tastingNote}</div>
+
+      <button onClick={() => props.changeView('BeerList')}> Back </button>
+      <button onClick={() => props.changeView('AddBeer')}> Edit Bier </button>
+      <button> Delete Bier </button>
     </div>
   );
 };
